d0f1237c6774ae8de4f0d954facf1c0411d69308
ChanchalShakyawal <105974768+ChanchalShakyawal@users.noreply.github.com>
2023-08-12 16:16:52 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <Box mt='80px' backgroundColor='#fff3f4'>
       <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
         <img src={Logo} alt="Logo" height='50px'/>
-        <Typography variant='h5' pb='40px' mt='20px'>Made with ❤️ by Rishabh Singh</Typography>
+        <Typography variant='h5' pb='40px' mt='20px'>Made with ❤️ by Chanchal Shakyawal</Typography>
       </Stack>
     </Box>
   );
